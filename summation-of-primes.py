0ac13968686188d8b2a996a9ceab73102adff4cd
George <georgekilburg@gmail.com>
2021-11-20 23:00:03 -0500

    summation-of-primes

--- summation-of-primes.py
+++ summation-of-primes.py
@@ -1,24 +1,27 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-prime_list = [2, 3, 5]
-prime = 0
-max = 2000000
-x = 1
+integers = [2]
+prime = 2
+cap = 200
+var = True
 
+for i in range(3, cap, 2):
+    integers.append(i)
 
-while prime < max:
-    prime = (6 * x) + 1
-    if prime < max:
-        prime_list.append(prime)
+while var is True:
+    for i in range(prime, max(integers), 2):
+        nonprime = i * prime
+        if nonprime > max(integers):
+            break
+        elif nonprime in integers:
+            integers.remove(nonprime)
 
-    prime = (6 * x) - 1
-    if prime < max:
-        prime_list.append(prime)
+    # print(prime)
 
-    x += 2
+    if integers.index(prime) + 1 < len(integers):
+        prime = integers[integers.index(prime) + 1]
+    else:
+        break
 
-prime_list = list(set(prime_list))   # eliminate duplicates
-prime_list.sort()
-print("Primes:", prime_list)
-print("Sum:", sum(prime_list))
+print(integers)
