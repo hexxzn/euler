c13ab11444a8071c7db7935cf402eb92a80cf57b
George <georgekilburg@gmail.com>
2021-11-22 01:50:22 -0500

    description update

--- largest-prime-factor.py
+++ largest-prime-factor.py
@@ -1,6 +1,5 @@
-# Prime factors are factors of a number that are, themselves, prime numbers.
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143?
+### Problem 3
+### https://projecteuler.net/problem=3
 
 from datetime import datetime
 startTime = datetime.now()
--- special-pythagorean-triplet.py
+++ special-pythagorean-triplet.py
@@ -1,7 +1,5 @@
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
-# For example, 3^2 + 4^2 = 5^2.
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product abc.
+### Problem 9
+### https://projecteuler.net/problem=9
 
 from datetime import datetime
 startTime = datetime.now()
