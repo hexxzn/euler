c952507eb7e4b9dcf99eba8c922aca28ab4280d2
George <georgekilburg@gmail.com>
2021-12-03 04:08:29 -0500

    solved problem 4 (java)

--- python/largest-palindrome-product.py
+++ python/largest-palindrome-product.py
@@ -4,20 +4,19 @@
 from datetime import datetime
 startTime = datetime.now()
 
-largest_palindrome = 0
+def largest_palindrome(length):
+    palindrome = 0
+    for x in range(1, int("9" * length) + 1):             
+        for y in range(1, int("9" * length) + 1):           
+            result = x * y      
+            if str(result) == str(result)[::-1] and result > palindrome:    # if result is palindrome and result is larger than largest palindrome, palindrome = result
+                palindrome = result
+    return palindrome
 
-for multiplicand in range(1, 1000):             
-    for multiplier in range(1, 1000):           
-        result = multiplier * multiplicand      
-        if str(result) == str(result)[::-1]:    # Check if result is palindrome by converting to string and comparing to reverse of itself
-            int(result)
-            if result > largest_palindrome:     # Check if confirmed palindrome is largest palindrome
-                largest_palindrome = result
-
-print(largest_palindrome)
+print(largest_palindrome(3))
 print(datetime.now() - startTime)
 
 # # # # # # # # # #
 # Answer: 906609  #
-# Time: 0:00.552  #
+# Time: 0:00.326  #
 # # # # # # # # # #
