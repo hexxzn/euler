df630939df284d3ef4d9b9a905c679cea0426bec
George <georgekilburg@gmail.com>
2022-10-26 04:34:42 -0400

    added solution 1

---
+++ java/Euler1.java
@@ -0,0 +1,15 @@
+public class Euler1 {
+    // https://projecteuler.net/problem=1
+    
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int n = 0; n < 1000; n += 1) {
+            if (n % 3 == 0 || n % 5 == 0) {
+                sum += n;
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
