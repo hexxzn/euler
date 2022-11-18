491c4648ffa88a1e9392344bd6cb0a40aa90832e
George <georgekilburg@gmail.com>
2022-11-17 23:08:41 -0500

    updated solution 1

--- java/Euler1.java
+++ java/Euler1.java
@@ -7,11 +7,13 @@ public class Euler1 {
 
     public static int multipleSum() {
         int sum = 0;
+
         for (int n = 0; n < 1000; n++) {
             if (n % 3 == 0 || n % 5 == 0) {
                 sum += n;
             }
         }
+        
         return sum;
      }
 }
