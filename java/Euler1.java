5273c03c702776b7f13804a18ef45e2748f6a817
George <georgekilburg@gmail.com>
2022-11-17 23:03:53 -0500

    updated solution 1

--- java/Euler1.java
+++ java/Euler1.java
@@ -2,14 +2,16 @@ public class Euler1 {
     // https://projecteuler.net/problem=1
     
     public static void main(String[] args) {
-        int sum = 0;
+        System.out.println("The sum of all multiples of 3 or 5 below 1000 is " + multipleSum() + ".");
+    }
 
+    public static int multipleSum() {
+        int sum = 0;
         for (int n = 0; n < 1000; n++) {
             if (n % 3 == 0 || n % 5 == 0) {
                 sum += n;
             }
         }
-
-        System.out.print(sum);
-    }
+        return sum;
+     }
 }
